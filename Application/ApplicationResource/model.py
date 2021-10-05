d59b27dd8481956cf3e78f9d65773e01ee513441
IgorDyslevski <igordyslevski@gmail.com>
2021-10-05 14:20:30 +0500

    Изменил README << добавил видео. Сделал просмотр видео с возможностью выбора лица

---
+++ Application/ApplicationResource/model.py
@@ -0,0 +1,164 @@
+import cv2
+import yoloface
+from tensorflow.keras.models import load_model
+from scipy.spatial.distance import cosine
+
+
+class Magician:
+    def __init__(self, path):
+        self.path = path
+        self.cascade = cv2.CascadeClassifier(self.path)
+        self.last_face_finded = False
+        self.last_face_coords = None
+        self.last_face_photo = None
+        self.face = yoloface.face_analysis()
+
+
+        # self.model = load_model('model.h5')
+
+    def set_last_face(self, coords, photo):
+        self.last_face_coords = coords
+        self.last_face_photo = photo
+        self.last_face_finded = True
+
+    def unset_last_face(self):
+        self.last_face_finded = False
+        self.last_face_coords = None
+        self.last_face_photo = None
+
+    def search_face_like_last_face_hist(self, faces_coords, photo):
+        results_of_getting_distance = []
+        last_face_x, last_face_y, last_face_w, last_face_h = self.last_face_coords
+        last_face = cv2.resize(
+            self.last_face_photo[last_face_y:last_face_y+last_face_h, last_face_x:last_face_x + last_face_w], (128, 128)
+        )
+        last_face_hist = cv2.calcHist([last_face], [0], None, [256],
+                                      [0, 256])
+        for x, y, w, h in faces_coords:
+            if x > -1 and y > -1:
+                face = cv2.resize(photo[y:y+h, x:x+w], (128, 128))
+            else:
+                if x < 0:
+                    x = 0
+                if y < 0:
+                    y = 0
+                face = cv2.resize(photo[y:y + h, x:x + w], (128, 128))
+            face_hist = cv2.calcHist([face], [0], None, [256],
+                                      [0, 256])
+
+            results_of_getting_distance.append(
+                cv2.compareHist(face_hist, last_face_hist, cv2.HISTCMP_BHATTACHARYYA)
+            )
+        index_of_min_differnce = results_of_getting_distance.index(min(results_of_getting_distance))
+        return index_of_min_differnce
+
+    # def search_face_like_last_face_vgg16(self, faces_coords, photo):
+    #     results_of_getting_distance = []
+    #     last_face_x, last_face_y, last_face_w, last_face_h = self.last_face_coords
+    #     last_face = cv2.resize(
+    #         self.last_face_photo[last_face_y:last_face_y+last_face_h, last_face_x:last_face_x + last_face_w], (224, 224)
+    #     )
+    #     last_face_emb = self.model.predict(last_face[None])
+    #     for x, y, w, h in faces_coords:
+    #         if x > -1 and y > -1:
+    #             face = cv2.resize(photo[y:y+h, x:x+w], (224, 224))
+    #         else:
+    #             if x < 0:
+    #                 x = 0
+    #             if y < 0:
+    #                 y = 0
+    #             face = cv2.resize(photo[y:y + h, x:x + w], (224, 224))
+    #         face_emb = self.model.predict(face[None])
+    #
+    #         results_of_getting_distance.append(
+    #             cosine(last_face_emb, face_emb)
+    #         )
+    #     index_of_min_differnce = results_of_getting_distance.index(min(results_of_getting_distance))
+    #     return index_of_min_differnce
+
+    def search_face_like_last_face_distance(self, faces_coords, photo):
+        height, width, channels = photo.shape
+        results_of_getting_distance = []
+        last_face_x, last_face_y, last_face_w, last_face_h = self.last_face_coords
+        last_face_central_x, last_face_central_y = last_face_x + last_face_w // 2, last_face_y + last_face_h // 2
+        for x, y, w, h in faces_coords:
+            central_x, central_y = x + w // 2, y + h // 2
+            results_of_getting_distance.append(
+                (abs(last_face_central_x - central_x) ** 2 + abs(last_face_central_y - central_y) ** 2) ** 0.5
+            )
+        index_of_min_differnce = results_of_getting_distance.index(min(results_of_getting_distance))
+        if results_of_getting_distance[index_of_min_differnce] > width // 5:
+            return -1
+        return index_of_min_differnce
+
+    def search_face_like_last_face_8x8(self, faces_coords, photo):
+        results_of_getting_distance = []
+        last_face_x, last_face_y, last_face_w, last_face_h = self.last_face_coords
+        last_face = cv2.resize(
+            self.last_face_photo[last_face_y:last_face_y+last_face_h, last_face_x:last_face_x + last_face_w], (8, 8)
+        )
+        for x, y, w, h in faces_coords:
+            if x > -1 and y > -1:
+                face = cv2.resize(photo[y:y+h, x:x+w], (8, 8))
+            else:
+                if x < 0:
+                    x = 0
+                if y < 0:
+                    y = 0
+                face = cv2.resize(photo[y:y + h, x:x + w], (8, 8))
+
+            res = []
+            # print(type(last_face - face))
+            # print(last_face - face)
+            for i in (last_face - face).reshape(-1):
+                res += abs(i)
+            results_of_getting_distance.append(
+                sum(res)
+            )
+        index_of_min_differnce = results_of_getting_distance.index(min(results_of_getting_distance))
+        return index_of_min_differnce
+
+    def search_faces_from_raw_photo_cv2(self, photo):
+        gray_photo = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+        faces_coords = self.cascade.detectMultiScale(gray_photo, 1.2, 1)
+        faces_coords = list(sorted(faces_coords, key=lambda x: x[2], reverse=True))
+        return faces_coords
+
+    def searhc_faces_from_raw_photo_yolo(self, photo):
+        _, box, conf = self.face.face_detection(frame_arr=photo, frame_status=True, model='tiny')
+        faces_coords = []
+        if box.__len__() > 0:
+            for item in box:
+                a = [item[0], item[1], item[3], item[2]]
+                faces_coords.append(a)
+        faces_coords = list(sorted(faces_coords, key=lambda x: (x[0], x[1])))
+        return faces_coords
+
+    def borders_coords(self, x, y, w, h, dpw, dph, width, height):
+        x_r = x + w + dpw - width - 1
+
+        if w == h:
+            pass
+        elif w > h:
+            dph += (w - h) // 2
+        elif h > w:
+            dpw += (h - w) // 2
+
+        xg, yg, wg, hg = x, y, w, h
+        x_r = x + w + dpw - width
+        y_r = y + h + dph - height
+        xr = 0
+        if x - dpw < 0:
+            xr = (x - dpw) * -1
+        xl = 0
+        if x + w + dpw - width > 0:
+            xl = x + w + dpw - width
+        yr = 0
+        if y - dph < 0:
+            yr = (y - dph) * -1
+        yl = 0
+        if y + h + dph - width > 0:
+            yl = y + h + dph - height
+
+        xg, yg, wg, hg = x - dpw + xr - xl, y - dph + yr - yl, x + w + dpw + xr - xl, y + h + dph + yr - yl
+        return xg, yg, wg, hg
--- Application/application.py
+++ Application/application.py
@@ -2,11 +2,15 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6 import QtWidgets
 from pyui.mainscreen import Ui_Form
 import sys
+import cv2
+from ApplicationResource.model import Magician
 
 # режим работы программы (0 - с записанным видео, 1 - online)
 mode = None
 # путь к видео (указывается, если mode = 0)
 path_video = None
+# путь к последующему видео
+path_result_video = None
 class App(Ui_Form, QWidget):
     def __init__(self):
         super().__init__()
@@ -14,14 +18,95 @@ class App(Ui_Form, QWidget):
         self.workswithvideobutton.clicked.connect(self.modeon0)
         self.recordvideowithprocess.clicked.connect(self.modeon1)
 
+        self.camera = None
+        self.mag = Magician('ApplicationResource/haarcascades/haarcascade_frontalface_default.xml')
+        self.big = True
+
     def modeon0(self):
-        global mode, path_video
+        global mode, path_video, path_result_video
         mode = 0
         path_video = QtWidgets.QFileDialog.getOpenFileName(self, 'Выберите файл')[0]
+        path_result_video = QtWidgets.QFileDialog.getSaveFileName(self, 'Выберите, куда сохранить файлы')[0]
+        self.camera = cv2.VideoCapture(path_video)
+        while True:
+            cv2.imshow('face', self.get_frame_video())
 
     def modeon1(self):
-        global mode
+        global mode, path_result_video
         mode = 1
+        path_result_video = QtWidgets.QFileDialog.getSaveFileName(self, 'Выберите, куда сохранить файлы')[0]
+
+    def gettext(self, text):
+        text, okPressed = QtWidgets.QInputDialog.getText(self, "Get text", text, QtWidgets.QLineEdit.Normal, "")
+        return text
+
+    def get_frame_video(self):
+        camera, big = self.camera, self.big
+        mag = self.mag
+        # getting ret and frame from camera
+        ret, frame = camera.read()
+        # getting face coordinates from camera
+        faces_coords = mag.searhc_faces_from_raw_photo_yolo(frame)
+        copy_frame = frame.copy()
+        main_face = None
+        # putting index and rectangle into frame
+        for i in range(1, len(faces_coords) + 1):
+            x, y, w, h = faces_coords[i - 1]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0))
+            cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2, cv2.LINE_AA)
+        if mag.last_face_finded == True:
+            if faces_coords.__len__() > 0:
+                index = mag.search_face_like_last_face_distance(faces_coords, copy_frame)
+                # chack for correct faces are
+                if index != -1:
+                    mag.set_last_face(faces_coords[index], copy_frame)
+                    dpw = 100
+                    dph = 100
+                    height, width, channels = copy_frame.shape
+                    x, y, w, h = faces_coords[index]
+                    xg, yg, wg, hg = mag.borders_coords(x, y, w, h, dpw, dph, width, height)
+                    main_face = copy_frame[yg:hg, xg:wg]
+                    main_face = cv2.resize(main_face, (height, height))
+                else:
+                    mag.unset_last_face()
+            else:
+                main_face = frame
+        else:
+            if big:
+                main_face = copy_frame
+        # displaying the frame
+        cv2.imshow('frame', frame)
+        # check for keyboard event
+        key_keyboard = cv2.waitKey(27)
+        try:
+            key = chr(key_keyboard)
+        except ValueError:
+            key = -1
+        if key != -1:
+            try:
+                try:
+                    print(key)
+                    mag.set_last_face(faces_coords[int(key) - 1], copy_frame)
+                except ValueError:
+                    if key == ' ':
+                        mag.unset_last_face()
+                        big = True
+            except ValueError:
+                print('Please, type a number')
+        if main_face is None:
+            rframe = frame
+        else:
+            rframe = main_face
+        h, w, _ = rframe.shape
+        if w != h:
+            if w > h:
+                rframe = cv2.copyMakeBorder(rframe, (w - h) // 2, (w - h) // 2, 0, 0, cv2.BORDER_CONSTANT, None,
+                                            value=0)
+            else:
+                rframe = cv2.copyMakeBorder(rframe, 0, 0, (h - w) // 2, (h - w) // 2, cv2.BORDER_CONSTANT, None,
+                                            value=0)
+        rframe = cv2.resize(rframe, (rframe.shape[0], rframe.shape[0]))
+        return rframe
 
 
 apl = QApplication(sys.argv)
