f7b883241de783d5611448f70ae7e0040c4d387d
IgorDyslevski <igordyslevski@gmail.com>
2021-11-02 12:18:03 +0500

    Новая программа и новы результаты работы

--- Application/application.py
+++ Application/application.py
@@ -3,7 +3,7 @@ from PyQt6 import QtWidgets
 from pyui.mainscreen import Ui_Form
 import sys
 import cv2
-from ApplicationResource.model import Magician
+from ApplicationResource.model import Witch, IncorrectModeCompareError, IncorrectSettingError, CameraPositionError
 
 # режим работы программы (0 - с записанным видео, 1 - online)
 mode = None
@@ -21,7 +21,8 @@ class App(Ui_Form, QWidget):
         self.recordvideowithprocess.clicked.connect(self.modeon1)
 
         self.camera = None
-        self.mag = Magician('ApplicationResource/haarcascades/haarcascade_frontalface_default.xml')
+        self.mag = Witch()
+        self.mag.set_for_searching_by_yolo()
         self.recording = False
         self.recorded = False
         self.big = True
@@ -79,8 +80,8 @@ class App(Ui_Form, QWidget):
         ret, frame = camera.read()
         # getting face coordinates from camera
         try:
-            faces_coords = mag.searhc_faces_from_raw_photo_yolo(frame)
-        except AttributeError:
+            faces_coords = mag.search_faces_from_raw_photo_yolo(frame)
+        except Exception as e:
             if self.recording:
                 self.videowritter.release()
                 print('saved')
@@ -93,21 +94,16 @@ class App(Ui_Form, QWidget):
             x, y, w, h = faces_coords[i - 1]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0))
             cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2, cv2.LINE_AA)
-        if mag.last_face_finded == True:
+        if mag.last_face_finded:
             if faces_coords.__len__() > 0:
-                index = mag.search_face_like_last_face_distance(faces_coords, copy_frame)
-                # chack for correct faces are
-                if index != -1:
-                    mag.set_last_face(faces_coords[index], copy_frame)
-                    dpw = 100
-                    dph = 100
-                    height, width, channels = copy_frame.shape
-                    x, y, w, h = faces_coords[index]
-                    xg, yg, wg, hg = mag.borders_coords(x, y, w, h, dpw, dph, width, height)
-                    main_face = copy_frame[yg:hg, xg:wg]
-                    main_face = cv2.resize(main_face, (height, height))
-                else:
-                    mag.unset_last_face()
+                if mag.camera_coords is None:
+                    mag.set_camera_coords((*mag.get_mean_coords(mag.last_face_coords),))
+                height, width, channels = copy_frame.shape
+                x, y, _, _, index = mag.move_camera_to_compare_face('d', faces_coords, copy_frame)
+                x, y = mag.make_stable((x, y), (600, 600), (width, height))
+                main_face = copy_frame[y - 300:y + 300, x - 300:x + 300]
+                # mag.last_data = x, y, w, h
+                main_face = cv2.resize(main_face, (height, height))
             else:
                 mag.unset_last_face()
                 main_face = frame
@@ -147,13 +143,7 @@ class App(Ui_Form, QWidget):
         else:
             rframe = main_face
         h, w, _ = rframe.shape
-        if w != h:
-            if w > h:
-                rframe = cv2.copyMakeBorder(rframe, (w - h) // 2, (w - h) // 2, 0, 0, cv2.BORDER_CONSTANT, None,
-                                            value=0)
-            else:
-                rframe = cv2.copyMakeBorder(rframe, 0, 0, (h - w) // 2, (h - w) // 2, cv2.BORDER_CONSTANT, None,
-                                            value=0)
+        rframe = mag.make_borders(rframe)
         rframe = cv2.resize(rframe, (rframe.shape[0], rframe.shape[0]))
         if self.recording:
             self.videowritter.write(cv2.resize(rframe, (self.resolution, self.resolution)))
